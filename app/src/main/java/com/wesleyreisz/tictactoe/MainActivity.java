bdf522f86acb49293ddc5c785104529710bbf88c
Wesley Reisz <wes@wesleyreisz.com>
2016-09-22 08:49:11 -0400

    Reorged some code & added Icons

--- app/src/main/java/com/wesleyreisz/tictactoe/MainActivity.java
+++ app/src/main/java/com/wesleyreisz/tictactoe/MainActivity.java
@@ -1,8 +1,5 @@
 package com.wesleyreisz.tictactoe;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,57 +32,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetGame();
     }
 
-    private void resetGame() {
-        Game game = Game.getInstance();
-        game.reset();
-        wireEvents();
-    }
-
-    private void wireEvents() {
+    private void wireEvents(boolean isGameBoardEnabled, boolean isResettingBoard) {
         ImageButton button1_1 = (ImageButton)findViewById(R.id.btn1_1);
-        button1_1.setEnabled(true);
+        button1_1.setEnabled(isGameBoardEnabled);
         button1_1.setOnClickListener(this);
-        button1_1.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button1_1.setImageResource(R.drawable.blank);
 
         ImageButton button1_2 = (ImageButton)findViewById(R.id.btn1_2);
-        button1_2.setEnabled(true);
+        button1_2.setEnabled(isGameBoardEnabled);
         button1_2.setOnClickListener(this);
-        button1_2.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button1_2.setImageResource(R.drawable.blank);
 
         ImageButton button1_3 = (ImageButton)findViewById(R.id.btn1_3);
-        button1_3.setEnabled(true);
+        button1_3.setEnabled(isGameBoardEnabled);
         button1_3.setOnClickListener(this);
-        button1_3.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button1_3.setImageResource(R.drawable.blank);
 
         ImageButton button2_1 = (ImageButton)findViewById(R.id.btn2_1);
-        button2_1.setEnabled(true);
+        button2_1.setEnabled(isGameBoardEnabled);
         button2_1.setOnClickListener(this);
-        button2_1.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button2_1.setImageResource(R.drawable.blank);
 
         ImageButton button2_2 = (ImageButton)findViewById(R.id.btn2_2);
-        button2_2.setEnabled(true);
+        button2_2.setEnabled(isGameBoardEnabled);
         button2_2.setOnClickListener(this);
-        button2_2.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button2_2.setImageResource(R.drawable.blank);
 
         ImageButton button2_3 = (ImageButton)findViewById(R.id.btn2_3);
-        button2_3.setEnabled(true);
+        button2_3.setEnabled(isGameBoardEnabled);
         button2_3.setOnClickListener(this);
-        button2_3.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button2_3.setImageResource(R.drawable.blank);
 
         ImageButton button3_1 = (ImageButton)findViewById(R.id.btn3_1);
-        button3_1.setEnabled(true);
+        button3_1.setEnabled(isGameBoardEnabled);
         button3_1.setOnClickListener(this);
-        button3_1.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button3_1.setImageResource(R.drawable.blank);
 
         ImageButton button3_2 = (ImageButton)findViewById(R.id.btn3_2);
-        button3_2.setEnabled(true);
+        button3_2.setEnabled(isGameBoardEnabled);
         button3_2.setOnClickListener(this);
-        button3_2.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button3_2.setImageResource(R.drawable.blank);
 
         ImageButton button3_3 = (ImageButton)findViewById(R.id.btn3_3);
-        button3_3.setEnabled(true);
+        button3_3.setEnabled(isGameBoardEnabled);
         button3_3.setOnClickListener(this);
-        button3_3.setImageResource(R.drawable.blank);
+        if (isResettingBoard)
+            button3_3.setImageResource(R.drawable.blank);
 
         Button btnReset = (Button)findViewById(R.id.btnReset);
         btnReset.setOnClickListener(new View.OnClickListener() {
@@ -106,8 +106,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void showWinner(int winner) {
         if(winner==Game.PLAYER1 || winner==Game.PLAYER2){
             Toast.makeText(this, "Player " + winner + " wins",Toast.LENGTH_SHORT).show();
+            wireEvents(false, false);
         }else if(winner==Game.DRAW){
             Toast.makeText(this, "Game was a Draw",Toast.LENGTH_SHORT).show();
+            wireEvents(false, false);
         }else{ /* nothing, keep playing */}
+    }
+    private void resetGame() {
+        Game game = Game.getInstance();
+        game.reset();
+        wireEvents(true, true);
     }
 }
